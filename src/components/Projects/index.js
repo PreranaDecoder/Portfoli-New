6326e1b9d1e23eb90c7af7af461c9bfcce06256e
Prerana Ramesh Bhandari <prerana.bhandari99@gmail.com>
2024-07-22 13:49:33 +0530

    2207

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -12,7 +12,7 @@ const Projects = ({openModal,setOpenModal}) => {
       <Wrapper>
         <Title>Projects</Title>
         <Desc>
-          I have worked on a wide range of projects. From web apps to android apps. Here are some of my projects.
+          I have worked on a wide range of projects. From web apps to web design. Here are some of my projects.
         </Desc>
         <ToggleButtonGroup >
           {toggle === 'all' ?
@@ -21,23 +21,18 @@ const Projects = ({openModal,setOpenModal}) => {
             <ToggleButton value="all" onClick={() => setToggle('all')}>All</ToggleButton>
           }
           <Divider />
-          {toggle === 'web app' ?
-            <ToggleButton active value="web app" onClick={() => setToggle('web app')}>WEB APP'S</ToggleButton>
+          {toggle === 'Web Design & Developement' ?
+            <ToggleButton active value="'Web Design & Developement" onClick={() => setToggle('web app')}>Web Design & Developement</ToggleButton>
             :
-            <ToggleButton value="web app" onClick={() => setToggle('web app')}>WEB APP'S</ToggleButton>
+            <ToggleButton value="'Web Design & Developement" onClick={() => setToggle('web app')}>Web Design & Developement</ToggleButton>
           }
           <Divider />
-          {toggle === 'android app' ?
-            <ToggleButton active value="android app" onClick={() => setToggle('android app')}>ANDROID APP'S</ToggleButton>
+          {toggle === 'UI/UX Design' ?
+            <ToggleButton active value="UI/UX Design" onClick={() => setToggle('android app')}>UI/UX Design</ToggleButton>
             :
-            <ToggleButton value="android app" onClick={() => setToggle('android app')}>ANDROID APP'S</ToggleButton>
+            <ToggleButton value="UI/UX Design" onClick={() => setToggle('android app')}>UI/UX Design</ToggleButton>
           }
-          <Divider />
-          {toggle === 'machine learning' ?
-            <ToggleButton active value="machine learning" onClick={() => setToggle('machine learning')}>MACHINE LEARNING</ToggleButton>
-            :
-            <ToggleButton value="machine learning" onClick={() => setToggle('machine learning')}>MACHINE LEARNING</ToggleButton>
-          }
+      
         </ToggleButtonGroup>
         <CardContainer>
           {toggle === 'all' && projects
